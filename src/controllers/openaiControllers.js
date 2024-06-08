9f24248304132267a4b7a9882cc2d6c114d9e401
Rafael Alexander Tena Chaparro <rafaelalexandertenachaparro@Rafael.local>
2024-06-08 10:46:08 -0600

    openai

--- src/controllers/openaiControllers.js
+++ src/controllers/openaiControllers.js
@@ -1,5 +1,48 @@
+import OpenAI from "openai";
+import dotenv from "dotenv";
+dotenv.config();
+
+const openai = new OpenAI();
+
 const getAllOpenai = async (_, res) => {
-  res.send(`GET ALL`);
+  try {
+    const thread = await openai.beta.threads.create();
+
+    await openai.beta.threads.messages.create(thread.id, {
+      role: "user",
+      content: "Hola! me puedes dar un dato curioso?",
+    });
+
+    let run = await openai.beta.threads.runs.createAndPoll(thread.id, {
+      assistant_id: process.env.OPENAI_ASSISTANT_ID,
+      instructions: "Be funny",
+    });
+
+    if (run.status === "completed") {
+      const messagesList = await openai.beta.threads.messages.list(
+        run.thread_id
+      );
+      const messages = messagesList.data.map((msg) => msg.content)[0][0].text
+        .value;
+
+      console.log("Jokes: ", messages);
+
+      res.status(200).send({
+        status: "success",
+        message: messages,
+      });
+    } else {
+      res.status(404).send({
+        status: run.status,
+        message: run,
+      });
+    }
+  } catch (error) {
+    res.status(500).send({
+      status: "error",
+      message: error.message,
+    });
+  }
 };
 
 const getOneOpenai = async (req, res) => {
@@ -9,13 +52,47 @@ const getOneOpenai = async (req, res) => {
 const createOpenai = async (req, res) => {
   const { body } = req;
 
-  if (!body.name) {
-    res.status(400).send("Name is required");
+  if (!body.message) {
+    res.status(400).send({
+      error: "Message is required",
+    });
   }
-  res.status(201).send({
-    status: "success",
-    message: "Openai created",
-  });
+  try {
+    const thread = await openai.beta.threads.create();
+
+    await openai.beta.threads.messages.create(thread.id, {
+      role: "user",
+      content: body.message,
+    });
+
+    let run = await openai.beta.threads.runs.createAndPoll(thread.id, {
+      assistant_id: process.env.OPENAI_ASSISTANT_ID,
+      instructions: "Be kind",
+    });
+
+    if (run.status === "completed") {
+      const messagesList = await openai.beta.threads.messages.list(
+        run.thread_id
+      );
+      const messages = messagesList.data.map((msg) => msg.content)[0][0].text
+        .value;
+
+      res.status(200).send({
+        status: "success",
+        message: messages,
+      });
+    } else {
+      res.status(404).send({
+        status: run.status,
+        message: run,
+      });
+    }
+  } catch (error) {
+    res.status(500).send({
+      status: "error",
+      message: error.message,
+    });
+  }
 };
 
 const updateOpenai = async (req, res) => {
