c62596d44f094afc83c4eeff903a9d0b8882401f
Rafael Alexander Tena Chaparro <rafaelalexandertenachaparro@Rafael.local>
2024-06-13 11:22:17 -0600

    cors

--- src/controllers/openai.controllers.js
+++ src/controllers/openai.controllers.js
@@ -33,6 +33,11 @@ const getOneOpenai = async (req, res) => {
 const createOpenai = async (req, res) => {
   const { body } = req;
 
+  console.log("body", body);
+
+  res.status(200).send(body);
+  return
+
   if (!body.message) {
     res.status(400).send({
       error: "Message is required",
